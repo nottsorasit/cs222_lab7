f236e89b1605d102a380e00ce0527692fce8c23f
nottsorasit <75597086+nottsorasit@users.noreply.github.com>
2022-02-27 23:48:28 +0700

    lab7

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    public void getAnimatorStand(){
+        this.GetComponent<Animator>().Play("Unarmed Idle Looking Ver_2");
+    }
+
+    public void getAnimatorWalk(){
+        this.GetComponent<Animator>().Play("Start Walking");
+    }
+}
